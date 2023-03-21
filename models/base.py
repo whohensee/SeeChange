8a474b52fccb963dbea0c44b4e7aa05ce10c885e
Guy Nir <guyynir@gmail.com>
2023-03-21 14:16:53 -0700

    add provenance and code version tables, add ORM base and database sessions

---
+++ models/base.py
@@ -0,0 +1,107 @@
+import os
+import sqlalchemy as sa
+from sqlalchemy import func
+
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
+utcnow = func.timezone("UTC", func.current_timestamp())
+
+
+# this is the root SeeChange folder
+CODE_ROOT = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+
+# this is where the data lives
+# (could be changed for, e.g., new external drive)
+DATA_ROOT = os.getenv("SEECHANGE_DATA")
+if DATA_ROOT is None:  # TODO: should also check if folder exists?
+    DATA_ROOT = os.path.join(CODE_ROOT, "output")
+
+DATA_TEMP = os.path.join(CODE_ROOT, "DATA_TEMP")
+
+# TODO: check with Rob if he has a preferred way of doing this
+# create database here:
+url = "postgresql://postgres:postgres@localhost:5432/astroretriever"
+engine = sa.create_engine(url, future=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+Session = sessionmaker(bind=engine)
+
+
+def safe_mkdir(path):
+
+    allowed_dirs = [
+        DATA_ROOT,
+        os.path.join(CODE_ROOT, "results"),
+        os.path.join(CODE_ROOT, "catalogs"),
+        DATA_TEMP,
+    ]
+
+    ok = False
+
+    for d in allowed_dirs:
+        parent = os.path.realpath(os.path.abspath(d))
+        child = os.path.realpath(os.path.abspath(path))
+
+        if os.path.commonpath([parent]) == os.path.commonpath([parent, child]):
+            ok = True
+            break
+
+    if not ok:
+        err_str = "Cannot make a new folder not inside the following folders: "
+        err_str += "\n".join(allowed_dirs)
+        err_str += f"\n\nAttempted folder: {path}"
+        raise ValueError(err_str)
+
+    # if the path is ok, also make the subfolders
+    os.makedirs(path, exist_ok=True)
+
+
+def clear_tables():
+    from models.provenance import CodeVersion, Provenance
+
+    try:
+        Provenance.metadata.drop_all(engine)
+    except:
+        pass
+
+    try:
+        CodeVersion.metadata.drop_all(engine)
+    except:
+        pass
+
+
+class SeeChangeBase:
+    """Base class for all SeeChange classes."""
+
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True,
+        doc="Unique identifier for this dataset",
+    )
+
+    created_at = sa.Column(
+        sa.DateTime,
+        nullable=False,
+        default=utcnow,
+        index=True,
+        doc="UTC time of insertion of object's row into the database.",
+    )
+
+    modified = sa.Column(
+        sa.DateTime,
+        default=utcnow,
+        onupdate=utcnow,
+        nullable=False,
+        doc="UTC time the object's row was last modified in the database.",
+    )
+
+
+Base = declarative_base(cls=SeeChangeBase)
+
+
+if __name__ == "__main__":
+    pass
---
+++ models/provenance.py
@@ -0,0 +1,97 @@
+import json
+import git
+import hashlib
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
+
+from models.base import Base
+
+
+class CodeVersion(Base):
+    __table_name__ = 'code_versions'
+
+    version = sa.Column(
+        sa.String,
+        nullable=False,
+        index=True,
+        unique=True,
+        doc='Version of the code. Can use semantic versioning or date/time, etc. '
+    )
+
+    commit_hashes = sa.Column(
+        sa.ARRAY(sa.String),
+        nullable=False,
+        doc='List of commit hashes that are included in this version of the code. '
+    )
+
+    def update(self):
+        repo = git.Repo(search_parent_directories=True)
+        git_hash = repo.head.object.hexsha
+        if git_hash not in self.commit_hashes:
+            new_hashes = self.commit_hashes + [git_hash]
+            self.commit_hashes = new_hashes
+
+
+class Provenance(Base):
+    __table_name__ = "provenances"
+
+    code_version_id = sa.Column(
+        sa.ForeignKey("code_versions.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+        doc="ID of the code version the provenance is associated with. ",
+    )
+
+    code_version = relationship(
+        "CodeVersion",
+        back_populates="provenances",
+        cascade="save-update, merge, expunge, refresh-expire",
+    )
+
+    parameters = sa.Column(
+        JSONB,
+        nullable=False,
+        default={},
+        doc="Critical parameters used to generate the underlying data. ",
+    )
+
+    upstream_ids = sa.Column(
+        sa.ARRAY(sa.Integer),
+        nullable=False,
+        default=[],
+        doc="IDs of the provenance rows used in upstream analysis. ",
+    )
+
+    unique_hash = sa.Column(
+        sa.String,
+        nullable=False,
+        index=True,
+        unique=True,
+        doc="Unique hash of the code version, parameters and upstream provenances used to generate this dataset. ",
+    )
+
+    def update_hash(self):
+        """
+        Update the unique_hash using the code_version, parameters and upstream_ids.
+        """
+        superdict = dict(
+            parameters=self.parameters,
+            upstream_ids=self.upstream_ids,
+            code_version=self.code_version.version
+        )
+        json_string = json.dumps(superdict, sort_keys=True)
+        self.unique_hash = hashlib.sha256(json_string.encode("utf-8")).hexdigest()
+
+
+CodeVersion.provenances = relationship(
+    "Provenance",
+    back_populates="code_version",
+    cascade="save-update, merge, expunge, refresh-expire",
+    foreign_keys="Provenance.code_version_id",
+    passive_deletes=True,
+)
+
+
+if __name__ == "__main__":
+    pass
