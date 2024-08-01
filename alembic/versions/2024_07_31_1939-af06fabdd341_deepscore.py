d287b6ae9065019621a883d751163da56f0a078c
whohensee <106775295+whohensee@users.noreply.github.com>
2024-08-01 12:19:07 -0700

    forgot to change down revision

--- alembic/versions/2024_07_31_1939-af06fabdd341_deepscore.py
+++ alembic/versions/2024_07_31_1939-af06fabdd341_deepscore.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'af06fabdd341'
-down_revision = '685ba6bab3f7'
+down_revision = '05bb57675701'
 branch_labels = None
 depends_on = None
 
